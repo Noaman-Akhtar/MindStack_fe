2c7a96987c596ffc34a190666fdce64e0b9bcf29
Noaman-Akhtar <akhtarnoaman@gmail.com>
2026-02-03 00:59:24 +0530

    solved one deployment issue

--- MindStack_be/override.d.ts
+++ MindStack_be/override.d.ts
@@ -4,7 +4,7 @@ import { Request } from 'express';
 declare global {
   namespace Express {
     interface Request {
-      userId?: mongoose.Types.ObjectId;  // Use proper mongoose type
+      userId?: string;
     }
   }
 }
--- MindStack_be/src/db.ts
+++ MindStack_be/src/db.ts
@@ -15,7 +15,7 @@ mongoose.connection.on('error', (err) => {
 });
 const UserSchema = new Schema({
   name: { type: String,unique:true, required: true },
-  password: { type: String, required: true},
+  password: { type: String, required: true}, 
 });
 export const UserModel = model('User', UserSchema);
 
